ed7cd52dd647ddc2d7b2be1ee13d68fd9be0aebe
ratherforky <jf16688@bristol.ac.uk>
2024-01-29 16:27:16 +0000

    added player with controls

--- src/Boidemic.hs
+++ src/Boidemic.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# language StrictData #-}
+{-# LANGUAGE LambdaCase #-}
 module Boidemic (boidemicMain) where
 
 import Apecs
@@ -22,6 +23,8 @@ import Flow
 import Data.String.Interpolate (i, __i)
 import Data.Fixed (mod')
 import Apecs.System (cmapIf)
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
 data Boid = Boid deriving Show
 instance Component Boid where type Storage Boid = Map Boid
@@ -34,21 +37,35 @@ instance Semigroup BoidSprite where
 instance Monoid BoidSprite where mempty = BoidSprite Blank
 instance Component BoidSprite where type Storage BoidSprite = Global BoidSprite
 
+newtype PlayerSprite = PlayerSprite Picture deriving Show
+instance Semigroup PlayerSprite where
+  PlayerSprite Blank <> p2 = p2
+  p1 <> PlayerSprite Blank = p1
+  p1 <> p2 = p1
+instance Monoid PlayerSprite where mempty = PlayerSprite Blank
+instance Component PlayerSprite where type Storage PlayerSprite = Global PlayerSprite
+
+
 newtype Position = Position (V2 Float) deriving Show
 instance Component Position where type Storage Position = Map Position
 
 newtype Velocity = Velocity (V2 Float) deriving Show
 instance Component Velocity where type Storage Velocity = Map Velocity
 
--- newtype Angle = Angle Float deriving Show -- Angle in radians
--- instance Component Angle where type Storage Angle = Map Angle
+newtype MaxSpeed = MaxSpeed Float deriving Show
+instance Component MaxSpeed where type Storage MaxSpeed = Map MaxSpeed
+
+newtype Angle = Angle Float deriving Show -- Angle in radians
+instance Component Angle where type Storage Angle = Map Angle
+
+data Player = Player deriving Show
+instance Component Player where type Storage Player = Unique Player
+
 
 -- newtype Speed = Speed Float deriving Show
 -- instance Component Speed where type Storage Speed = Map Speed
 
 
--- data Player = Player deriving Show
--- instance Component Player where type Storage Player = Unique Player
 
 -- newtype Score = Score Int deriving (Show, Num)
 -- instance Semigroup Score where (<>) = (+)
@@ -70,20 +87,64 @@ instance Component Velocity where type Storage Velocity = Map Velocity
 -- instance Semigroup RightDown where RightDown p <> RightDown q = RightDown (p && q)
 -- instance Monoid RightDown where mempty = RightDown False
 
-makeWorld "World" [''Camera, ''Boid, ''BoidSprite, ''Position, ''Velocity]
+newtype KeySet = KeySet { unKeySet :: S.Set Key } deriving Show
+instance Semigroup KeySet where
+  ksX <> ksY = KeySet (unKeySet ksX `S.union` unKeySet ksY)
+instance Monoid KeySet where mempty = KeySet S.empty
+instance Component KeySet where type Storage KeySet = Global KeySet
+
+makeWorld "World" [''Camera, ''Player, ''Boid, ''BoidSprite, ''PlayerSprite, ''Position, ''Velocity, ''MaxSpeed, ''Angle, ''KeySet]
 
 type System' a = System World a
-type Kinetic = (Position, Velocity)
+type Kinetic = (Position, Velocity, MaxSpeed, Angle)
+
+
+-------------------------------------------
+-- User input
+-------------------------------------------
+
+data Action
+  = LeftA
+  | RightA
+  | UpA
+  | DownA
+  deriving (Show, Eq, Ord, Enum)
+
+type KeyBindings = M.Map Key Action
+
+defaultKeyBindings :: KeyBindings
+defaultKeyBindings = M.fromList
+  [ (SpecialKey KeyUp, UpA)
+  , (SpecialKey KeyLeft, LeftA)
+  , (SpecialKey KeyDown, DownA)
+  , (SpecialKey KeyRight, RightA)
+  , (Char 'w', UpA)
+  , (Char 'a', LeftA)
+  , (Char 's', DownA)
+  , (Char 'd', RightA)
+  ]
+
+
+handleEvent :: Event -> System' ()
+handleEvent (EventKey (SpecialKey KeyEsc) Down   _ _) = liftIO exitSuccess
+handleEvent (EventKey k Down _ _) = modify global $ \(KeySet ks) -> KeySet (S.insert k ks)
+handleEvent (EventKey k Up _ _)   = modify global $ \(KeySet ks) -> KeySet (S.delete k ks)
+handleEvent _ = return ()
+
+-------------------------------------------
+-- Magic numbers
+-------------------------------------------
 
 
 -- type Kinetic = (Position, Velocity)
 -- , ymin, ymax :: Double
-areaWidth, areaHeight, maxSpeed, separationDist :: Float
+areaWidth, areaHeight, boidMaxSpeed, playerMaxSpeed, separationDist :: Float
 areaWidth = 400
 areaHeight = 400
 -- xmax = (areaWidth - playerW) / 2 - 5
 -- xmin = -xmax
-maxSpeed = 100
+boidMaxSpeed = 100
+playerMaxSpeed = 100
 sightRadius = 100
 separationDist = 30
 
@@ -92,43 +153,24 @@ cohesionFactor = 0.02
 separationFactor = 0.1
 alignmentFactor = 0.1
 
+
+-------------------------------------------
+-- Spawning entities
+-------------------------------------------
+
 newBoid :: V2 Float -> V2 Float -> System' ()
 newBoid pos v
-  = newEntity_ (Boid, Position pos, Velocity v)
+  = newEntity_ (Boid, Position pos, Velocity v, Angle 0, MaxSpeed boidMaxSpeed)
 
+initPlayer :: V2 Float -> System' ()
+initPlayer pos
+  = newEntity_ (Player, Position pos, Velocity 0, Angle 0, MaxSpeed playerMaxSpeed)
 
+-------------------------------------------
+-- Game Logic
+-------------------------------------------
 
-
-
-logDebug :: String -> System' ()
--- logDebug = liftIO . putStrLn
-logDebug _ = pure ()
-
-toRadians :: Floating a => a -> a
-toRadians x = x * (pi / 180)
-
-clamp :: Ord a => a -> a -> a -> a
-clamp minVal maxVal x = min maxVal (max minVal x)
-
--- Particle effects systems
-
-stepPosition :: Float -> System' ()
-stepPosition dT
-  = cmap $ \(Velocity v, Position p) -> Position (p + dT *^ v)
-
--- spawnParticles :: Int -> V2 Float -> (Float, Float) -> (Float, Float) -> System' ()
--- spawnParticles n pos dvx dvy = replicateM_ n $ do
---   vx <- liftIO $ randomRIO dvx
---   vy <- liftIO $ randomRIO dvy
---   t  <- liftIO $ randomRIO (0.02,0.3)
---   particle <- newEntity (Particle t, Position pos, Velocity (V2 vx vy))
---   logDebug [__i|New particle: #{particle}|]
-
--- setPlayerVelocity :: System' ()
--- setPlayerVelocity = cmap $ \(Player, LeftDown ld, RightDown rd) -> 
---   if |     ld && not rd -> Velocity (V2 -playerSpeed 0)
---      | not ld &&     rd -> Velocity (V2  playerSpeed 0)
---      | otherwise        -> Velocity (V2 0 0)
+-- This page is a particularly useful point of reference: http://www.kfish.org/boids/pseudocode.html
 
 step :: Float -> System' ()
 step dT = do
@@ -146,18 +188,18 @@ step dT = do
   -- stepParticles dT
   -- handleCollisions
 
--------------------------------------------
--- Game Logic
--------------------------------------------
-
--- This page is a particularly useful point of reference: http://www.kfish.org/boids/pseudocode.html
+stepPosition :: Float -> System' ()
+stepPosition dT
+  = cmap $ \(Velocity v, Position p) -> Position (p + dT *^ v)
 
 gameLogic :: System' ()
 gameLogic = do
   cohesion
   separation
   alignment -- boids are zooming off. need a speed limit
+  playerVelocity
   speedLimit
+  syncAngle
 
 cohesion :: System' ()
 cohesion = cmapM $ \(Boid, Position p, Velocity v) -> do
@@ -194,42 +236,43 @@ alignment = cmapM $ \(Boid, Position p, Velocity v) -> do
               in  v + dv
   pure $ Velocity v'
 
-withinRange :: Float -> V2 Float -> V2 Float -> Bool
-withinRange radius p1 p2 = distance p1 p2 <= radius
-
 speedLimit :: System' ()
 speedLimit
-  = cmapIf (\(Velocity v) -> norm v > maxSpeed)
-           (\(Boid, Velocity v) -> Velocity (maxSpeed *^ signorm v))
+  = cmapIf (\(Velocity v, MaxSpeed s) -> norm v > s)
+           (\(Boid, Velocity v, MaxSpeed s) -> Velocity (s *^ signorm v))
 
-toMaybe :: Bool -> a -> Maybe a
-toMaybe q x = if q then Just x else Nothing
+syncAngle :: System' ()
+syncAngle
+  = cmapIf (\(Velocity v) -> v /= 0) -- Prevent div by 0
+           (\(Angle _, Velocity v) -> Angle (unangle v))
+
+playerVelocity :: System' ()
+playerVelocity = do
+  KeySet ks <- get global
+  let actions = S.foldr' (\k acc -> maybe acc -- If key not in bindings, don't insert
+                                          (`S.insert` acc)
+                                          (defaultKeyBindings M.!? k))
+                         S.empty
+                         ks
+
+      vs = S.toList actions
+            |> map actionToVelocity
+            |> sum
+
+  cmap $ \(Player, Velocity v) -> Velocity (playerMaxSpeed *^ vs)
+
+actionToVelocity :: Action -> V2 Float
+actionToVelocity = \case
+  UpA    -> V2  0  1
+  LeftA  -> V2 -1  0
+  DownA  -> V2  0 -1
+  RightA -> V2  1  0
+
+
 
 -------------------------------------------
 -- Rest
 -------------------------------------------
-
-handleEvent :: Event -> System' ()
--- handleEvent (EventKey (SpecialKey KeyLeft) Down _ _) =
---   set global (LeftDown True)
-
--- handleEvent (EventKey (SpecialKey KeyLeft)  Up   _ _) =
---   set global (LeftDown False)
-
--- handleEvent (EventKey (SpecialKey KeyRight) Down _ _) =
---   set global (RightDown True)
-
--- handleEvent (EventKey (SpecialKey KeyRight) Up   _ _) =
---   set global (RightDown False)
-
--- -- handleEvent (EventKey (SpecialKey KeySpace) Down _ _) =
--- --   cmapM_ $ \(Player, pos) -> do
--- --     _ <- newEntity (Ball, pos, Velocity (V2 0 ballSpeed))
--- --     spawnParticles 7 pos (-80,80) (10,100)
-
-handleEvent (EventKey (SpecialKey KeyEsc) Down   _ _) = liftIO exitSuccess
-
-handleEvent _ = return ()
 
 translate' :: V2 Float -> Picture -> Picture
 translate' (V2 x y) = translate x y
@@ -242,9 +285,14 @@ radToDegreeFactor = 180 / pi
 
 draw :: System' Picture
 draw = do
-  foldDrawM $ \(Boid, Position pos, Velocity v) -> do
-                 BoidSprite sprite <- get global
-                 pure (translate' pos $ rotate' (unangle v) sprite)
+  BoidSprite boidSprite <- get global
+  PlayerSprite playerSprite <- get global
+  boids <- foldDraw $ \(Boid, Position pos, Angle a) ->
+                translate' pos $ rotate' a boidSprite
+  player <- foldDraw $ \(Player, Position pos, Angle a) ->
+                translate' pos $ rotate' a playerSprite
+  
+  pure $ boids <> player
 
 display :: Display
 display = InWindow "Boidemic" (640, 640) (10, 10)
@@ -259,15 +307,18 @@ initialise :: System' ()
 initialise = do
   set global ( Camera 0 1 )
   randomSpawnBoids 5
+  initPlayer (V2 0 0)
 
 
 boidemicMain :: IO ()
 boidemicMain = do
   w <- initWorld
   boidSprite <- loadBMP "src/boid.bmp"
+  playerSprite <- loadBMP "src/player.bmp"
   setStdGen (mkStdGen 0)
   runWith w $ do
     set global (BoidSprite boidSprite)
+    set global (PlayerSprite playerSprite)
     initialise
     play display
          black
@@ -276,3 +327,22 @@ boidemicMain = do
          handleEvent
          step
 
+-------------------------------------------
+-- Utility
+-------------------------------------------
+
+logDebug :: String -> System' ()
+-- logDebug = liftIO . putStrLn
+logDebug _ = pure ()
+
+toRadians :: Floating a => a -> a
+toRadians x = x * (pi / 180)
+
+clamp :: Ord a => a -> a -> a -> a
+clamp minVal maxVal x = min maxVal (max minVal x)
+
+toMaybe :: Bool -> a -> Maybe a
+toMaybe q x = if q then Just x else Nothing
+
+withinRange :: Float -> V2 Float -> V2 Float -> Bool
+withinRange radius p1 p2 = distance p1 p2 <= radius
